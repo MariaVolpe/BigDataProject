2cdc7af1b96c7e8560752f6318e6c71ca230530b
Maria Volpe <mariaCHVolpe@gmail.com>
2018-04-06 20:55:35 -0400

    Update main function

--- main.py
+++ main.py
@@ -1,52 +1,51 @@
 from pymongo import MongoClient
 import loaddata
 import querydata
-import 
+#import 
 
 client = MongoClient()
 
 class Interaction:
 
     def __init__(self):
-        dummy = 0
+        #DB: GeneInformation
+        self.db = client.GeneInformation
+        #collection: genes
+        self.genes = self.db.genes
 
     def load(self):
         obj = loaddata.LoadData()
-        obj.load_files()
+        #obj.load_files()
+        print("testing mode. feature not allowed")
 
-    def find_stats(self, entrez_id):
+    def get_stats(self, entrez_id):
         query_obj = querydata.QueryData()
         query_obj.get_all_information(entrez_id)
 
-    def find_associated(entrez_id, n):
+    def get_associated(entrez_id, n):
         dummy = 0
 
-    def find_patient(patient_id):
+    def get_patient(patient_id):
         dummy = 0
 
+    def find(dictionary):
+        self.genes.find(dictionary)
 
 def main():
 
-    #DB: GeneInformation
-    db = client.GeneInformation
-    #collection: genes
-    genes = db.genes
-
     obj = Interaction()
-    
 
-    run == True
+    run = True
     while(run==True):
         print("Enter input:")
         s = input()
-        if s == "quit":
-            run == False
+        s = s.strip()
+        if "quit" in s:
+            run = False
         elif "drop" in s:
             print("Please don't delete anything in the database.")
-        elif "load" or "find" in s:
-            s = "obj." + s
-            eval(s)
         else:
+            s = "obj." + s
             eval(s)
     
 
